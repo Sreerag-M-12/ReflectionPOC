06226fe1b0d9e1f373ad8e8bf17a8321300c3c93
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-20 15:12:29 +0530

    innit

---
+++ Models/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionPOC.Models
+{
+    internal class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+            Console.WriteLine("constructor default");
+        }
+        public Account(int id, string name, double balance)
+        {
+            Id = id; 
+            Name = name; 
+            Balance = balance;
+        }
+
+        public string ShowInfo()
+        {
+            return $"ID: {Id}" +
+                $"Name: {Name}" +
+                $"Balance: {Balance}";
+        }
+        public string CheckBalance(Account acc)
+        {
+            if (acc.Balance > 500)
+                return "Has more than min balance \n";
+            else
+                return "Doesnt hve min balance";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using ReflectionPOC.Models;
+
+namespace ReflectionPOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myClassType = typeof(Account);
+
+           
+            PropertyInfo[] properties = myClassType.GetProperties();
+            Console.WriteLine($"Number of properties: {properties.Length}");
+
+
+
+            MethodInfo[] methods = myClassType.GetMethods
+                (BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+            Console.WriteLine($"Number of methods: {methods.Length}");
+
+
+            ConstructorInfo[] constructors = myClassType.GetConstructors();
+            Console.WriteLine($"Number of constructors: {constructors.Length}");
+        }
+    }
+}
